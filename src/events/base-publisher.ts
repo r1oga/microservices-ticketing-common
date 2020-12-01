4d11e6d9acf25ffbdc5f5f4f5f4e3465021d0525
r1oga <r1oga@tuta.io>
2020-12-01 14:59:50 +0100

    Updates

--- src/events/base-publisher.ts
+++ src/events/base-publisher.ts
@@ -5,7 +5,7 @@ import { Event } from './interfaces'
 export abstract class Publisher<T extends Event> {
   abstract subject: T['subject']
 
-  constructor(private client: Stan) {}
+  constructor(protected client: Stan) {}
 
   publish(data: T['data']): Promise<void> {
     return new Promise((resolve, reject) => {
